4ddd395de3667732e13127317a1e55bb2851df8c
ca6.reggie <ca6.reggie@enfrausaph.com>
2018-05-08 05:51:48 +0800

    added animation, add and remove item is now working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+
+  	trigger('todos',  [
+  		transition('* => *', [
+  			query(':enter', style({opacity: 0}), {optional:true}),
+
+  			query(':enter', stagger('300ms', [
+  				animate('.6s ease-in', keyframes([
+  					style({opacity: 0, transform: 'translateY( -75%)', offset: 0}),
+  					style({opacity: .5, transform: 'translateY( 35px)', offset: .3}),
+  					style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+  				]))
+  			]),{optional:true}),
+
+  			query(':leave', stagger('300ms', [
+  				animate('.6s ease-in', keyframes([
+  					style({opacity: 0, transform: 'translateY(0)', offset: 0}),
+  					style({opacity: .5, transform: 'translateY( 35px)', offset: .3}),
+  					style({opacity: 1, transform: 'translateY(-75%)', offset: 1}),
+  				]))
+  			]),{optional:true})  			
+  		])
+  	])
+
+  ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	itemCount: number;
+	btnText: string = 'Add an item';
+	todoText: string;
+	todo = [];
 
-  ngOnInit() {
-  }
 
+	constructor() { }
+
+	ngOnInit() {
+		this.itemCount = this.todo.length;
+	}
+
+	addItem(){
+		this.todo.push(this.todoText);
+		this.todoText = '';
+		this.itemCount = this.todo.length;
+
+	}
+	removeItem(i){
+				
+			this.todo.splice(i,1);
+			this.itemCount = this.todo.length;
+	}
 }
